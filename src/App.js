a2db89eb76352c629be96469eca5f1257d605c7e
ZeroPlusKikkok <49876888+ZeroPlusKikkok@users.noreply.github.com>
2022-01-26 14:23:32 +0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Template from './components/Template';
+
+function App() {
+  return (
+    <div className="App">
+      <Template />
+    </div>
+  );
+}
+
+export default App;
