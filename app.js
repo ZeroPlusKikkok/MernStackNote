7c6b302f6ea0f2d72a6fca7efd4d71d08d549d5a
ZeroPlusKikkok <49876888+ZeroPlusKikkok@users.noreply.github.com>
2022-09-01 12:02:37 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,83 +1,111 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
+const appConfig = require('./config.js');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const compression = require('compression');
+const express = require('express');
+const expressSession = require('express-session');
+const favicon = require('serve-favicon'); // eslint-disable-line
+const helmet = require('helmet');
+const LocalStrategy = require('passport-local').Strategy;
 const logger = require('morgan');
 const mongoose = require('mongoose');
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const expressSession = require('express-session')({
-  secret: 'some random string goes here',
-  resave: false,
-  saveUninitalzed: false,
-});
-
+const path = require('path');
+const RateLimit = require('express-rate-limit');
 const webpack = require('webpack');
 const webpackConfig = require('./webpack.config');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 
+const User = require('./models/user');
+
+// Route Files
+const api = require('./routes/api/index');
+const albums = require('./routes/api/albums');
+const artists = require('./routes/api/artists');
+const authentication = require('./routes/api/authentication');
 const index = require('./routes/index');
 const users = require('./routes/api/users');
-const api = require('./routes/api/index');
-const authentication = require('./routes/api/authentication');
 
 const app = express();
 
 // Connect Mongoose
-mongoose.connect('mongodb://localhost/myDatabase', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => console.log('Successfully connected to MongoDB...'))
-  .catch((err) => console.log(err));
+mongoose.connect(`mongodb://${appConfig.mongodb.user}:${appConfig.mongodb.password}@localhost/musiclist?authSource=admin`, { useMongoClient: true });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// uncomment after placing your favicon in /public
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(expressSession);
+app.use(compression());
+// Express Session
+const sessionValues = {
+  cookie: {},
+  name: 'sessionId',
+  resave: false,
+  saveUninitialized: true,
+  secret: appConfig.expressSession.secret,
+};
+if (app.get('env') === 'production') {
+  app.set('trust proxy', 1);
+  // sessionValues.cookie.secure = true;
+}
+app.use(expressSession(sessionValues));
+app.use(helmet());
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Configure passport
-const User = require('./models/user');
+// Webpack Server
+if (process.env.NODE_ENV !== 'production') {
+  const webpackCompiler = webpack(webpackConfig);
+  app.use(webpackDevMiddleware(webpackCompiler, {
+    publicPath: webpackConfig.output.publicPath,
+    stats: {
+      colors: true,
+      chunks: true,
+      'errors-only': true,
+    },
+  }));
+  app.use(webpackHotMiddleware(webpackCompiler, {
+    log: console.log,
+  }));
+}
 
+// configure rate limiter
+const apiLimiter = new RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 50,
+  delayMs: 0, // disabled
+});
+app.use('/api/', apiLimiter);
+
+app.use('/api', api);
+app.use('/api/albums', albums);
+app.use('/api/artists', artists);
+app.use('/api/authentication', authentication);
+app.use('/api/users', users);
+app.use('/*', index);
+
+// Configure Passport
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-// Webpack Server
-const webpackCompiler = webpack(webpackConfig);
-app.use(webpackDevMiddleware(webpackCompiler, {
-  publicPath: webpackConfig.output.publicPath,
-  stats: {
-    colors: true,
-    chunks: true,
-    'errors-only': true,
-  },
-}));
-app.use(webpackHotMiddleware(webpackCompiler, {
-  log: console.log,
-}));
-
-app.use('/api/authentication', authentication);
-app.use('/api/users', users);
-app.use('/api', api);
-app.use('/*', index);
-
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  next(createError(404));
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
-app.use((err, req, res, next) => { // eslint-disable-line
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
